8c483fc889ca6d2cb179e181d53d6dbb55373dce
thangtranit1999 <ngocthangkt27@gmail.com>
2020-07-21 01:18:40 +0700

    abc

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const https = require('https');
 const fs = require('fs');
+const express = require('express');
+const app = express();
 
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
+
+app.use('/tesst', (req, res)=>{
+    res.send({
+        message:"okok"
+    })
+})
 
 https.createServer(options, function (req, res) {
   res.writeHead(200);
